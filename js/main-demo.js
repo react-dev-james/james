ad8745b79d5360687f0f078a3833bf775b4aa2e3
seniordev92 <yegoryanushkevich@gmail.com>
2022-12-09 12:16:32 +0900

    Update

--- js/main-demo.js
+++ js/main-demo.js
@@ -1,14 +1,5 @@
 $(function() {
 
-    $('.theme-options').addClass('active');
-    setTimeout(function(){
-        $('.theme-options').removeClass('active');
-    }, 5000);
-
-    $('.theme-options .toggle-btn').on('click', function() {
-        $('.theme-options').toggleClass('active');
-    });
-
     $('.theme-color li a').on('click', function(e) {
         var style_link = $(this).attr('href');
         $('link.site-color').attr('href', style_link);
